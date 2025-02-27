e59c813ac83f4e831dd42eca902ac660f6aeb623
Henry Wallace <henry.wallace@rhul.ac.uk>
2025-02-27 15:00:22 +0000

    removes watchdog dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
         "textual",
         "textual_dev",
         "rich",
-        "watchdog",
     ],
     extras_require={"develop": ["ipdb", "ipython"]},
     package_data={"": ["*.tcss", "*.yml"]},
