84a65d0f9ca4898e520fd256cce94e2abd74840c
Henry Wallace <henry.wallace@rhul.ac.uk>
2025-04-02 15:54:49 +0100

    remove unnecessary setup file

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-HERE=${BASH_SOURCE[0]:-${(%):-%x}}
-export CIDER=$(cd $(dirname ${HERE}) && pwd)
-echo "Setting up CIDER from $CIDER_DIR`"
-cd $CIDER_DIR && pip install -e . && cd -
-# export CIDER_DATA=${CIDER_DIR}/data
-echo "CIDER setup done"
