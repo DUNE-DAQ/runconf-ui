350c31f79695a2b5a63d7d4bfd518eb9bb87d71d
MRiganSUSX <M.Rigan@sussex.ac.uk>
2025-07-04 10:45:45 +0200

    properly setting operation url env var

--- scripts/setup_np04_environment.sh
+++ scripts/setup_np04_environment.sh
@@ -5,6 +5,6 @@ export APPARATUS="np04"
 export SESSION_FILE="np04-session.data.xml"
 export SESSION_NAME="np04-session"
 export CONFIG_DIR="${DBT_AREA_ROOT}/np04-runs"
-export OPERATION_URL="ssh://git@gitlab.cern.ch:7999/dune-daq/online/np04-configs-operation.git"
+export OPERATION_URL="https://gitlab.cern.ch/dune-daq/online/np04-configs-operation.git"
 
 # export BASE_URL="ssh://git@gitlab.cern.ch:7999/dune-daq/online/ehn1-daqconfigs.git"
