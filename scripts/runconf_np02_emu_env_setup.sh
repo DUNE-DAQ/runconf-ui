c4a8c3a7ef3b4e1c538968eb6ac6089507a019b2
MRiganSUSX <M.Rigan@sussex.ac.uk>
2026-03-24 16:20:47 +0100

    emu scripts

---
+++ scripts/runconf_np02_emu_env_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export APPARATUS="np02-emu"
+
+export SESSION_FILE="np02-emu-session.data.xml"
+export SESSION_NAME="np02-emu-session"
+export CONFIG_DIR="${DBT_AREA_ROOT}/np02-runs"
+
---
+++ scripts/runconf_np04_emu_env_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export APPARATUS="np04-emu"
+
+export SESSION_FILE="np04-emu-session.data.xml"
+export SESSION_NAME="np04-emu-session"
+export CONFIG_DIR="${DBT_AREA_ROOT}/np04-runs"
+
