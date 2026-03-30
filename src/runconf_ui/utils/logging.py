8113b02c56ca6742319eef10061ef6eb49889b27
Henry Wallace <henry.wallace@rhul.ac.uk>
2026-03-30 14:48:51 +0100

    fix most logging visuals

--- src/runconf_ui/utils/logging.py
+++ src/runconf_ui/utils/logging.py
@@ -9,28 +9,27 @@ from runconf_ui.exceptions import LoggerNotFound
 LogLevels = Literal["INFO", "DEBUG", "WARNING"]
 
 _LOGGER_NAME = "runconf_ui_logger"
-_logger = None
-
+_LOGGER = None
 
 def init_logger(log_file: Path, log_level: LogLevels = "INFO"):
-    """Call once at application startup."""
-    global _logger
-    if _logger is not None:
+    global _LOGGER
+    if _LOGGER is not None:
         return
 
     log_file.parent.mkdir(parents=True, exist_ok=True)
-    _logger = get_daq_logger(
+    _LOGGER = get_daq_logger(
         logger_name=_LOGGER_NAME,
         log_level=log_level,
-        use_parent_handlers=True,
-        rich_handler=True,
-        stream_handlers=True,
+        use_parent_handlers=False,  # was True
+        rich_handler=False,
+        stream_handlers=False,
         file_handler_path=str(log_file),
     )
+    _LOGGER.propagate = False  # belt-and-braces
 
 
 def get_logger():
-    if _logger is None:
+    if _LOGGER is None:
         raise LoggerNotFound("cannot find logger!")
 
-    return _logger
+    return _LOGGER
