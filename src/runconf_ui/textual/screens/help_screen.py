ce864104cb3c81c5358804b47b8a8c9467074372
Henry Wallace <henry.wallace@rhul.ac.uk>
2026-04-09 15:50:13 +0100

    Fix help text using rich

--- src/runconf_ui/textual/screens/help_screen.py
+++ src/runconf_ui/textual/screens/help_screen.py
@@ -1,6 +1,6 @@
-from textwrap import dedent
 from typing import ClassVar
 
+from rich.text import Text
 from textual.containers import ScrollableContainer, Vertical
 from textual.screen import ModalScreen
 from textual.widgets import Button, Static
@@ -21,50 +21,62 @@ class HelpScreen(ModalScreen):
             )
 
     def message(self):
-        """Generate the help message text.
+        text = Text()
 
-        :returns: Formatted help text with Rich markup
-        :rtype: str
-        """
-        return dedent("""\
-            [bold]Hello and welcome to Runconf-shifter UI!![/bold]
-            To get started, please select the following from dropdown menus:
-                1. The DAQ version you would like to use 
-                2  Once this is selected, select the session you would like to access
-                3. Press Open
-            
-            This will spawn 2 panels
-            1. The Enable/Disable Panel (left)
-            2. The map/adjustable attribute panel (right)
+        # Title
+        text.append("Hello and welcome to Runconf-shifter UI!!\n", style="bold")
 
-            [bold]Enable Disable Panel[/bold]
-            These tabs contain everything in the detector that can be toggled on/off. 
-            To toggle an object on/off press the button. If a parent of the object
-            is switched off, the buttton will not be pressable.
-    
-            To see these relationships you can use the map panels.
-            
-            [bold]Map Panels[/bold]
-            The panels on the right contain 3 elements. We will first look at
-            the "System Maps" and "Configuration" views. 
-            
-            Configuration is an overview of a large subset of objects within the
-            detector. It gives you a global view of what's currently enable/disabled
-            
-            The System Maps give you maps of elements in the Enable/Disable Panel. 
-            This can tell you exactly what the panels are turning on and off.
-            
-            [bold]Adjustable Attributes[/bold]
-            Finally, we can look at the adjustable attributes. Adjustable attributes
-            are things we want to set the value of before the run, for example,
-            trigger rates.
-            
-            [bold]Creating a Configuration[/bold]
-            Once you're done modifying the configuration, simply press "create".
-            This will generate a configuration file and your terminal
-            will tell you how to use this with DRUNC.
+        # Intro
+        text.append(
+            "\nTo get started, please select the following from dropdown menus:\n"
+            "  1. The DAQ version you would like to use\n"
+            "  2. Once this is selected, select the session you would like to access\n"
+            "  3. Press Open\n"
+        )
 
-        """)
+        # Panels
+        text.append(
+            "\nThis will spawn 2 panels:\n"
+            "  1. The Enable/Disable Panel (left)\n"
+            "  2. The map/adjustable attribute panel (right)\n"
+        )
+
+        # Enable/Disable Panel
+        text.append("\nEnable Disable Panel\n", style="bold")
+        text.append(
+            "These tabs contain everything in the detector that can be toggled on/off. "
+            "To toggle an object on/off press the button. If a parent of the object "
+            "is switched off, the button will not be pressable.\n\n"
+            "To see these relationships you can use the map panels.\n"
+        )
+
+        # Map Panels
+        text.append("\nMap Panels\n", style="bold")
+        text.append(
+            'The panels on the right contain 3 elements. We will first look at the "System Maps" '
+            'and "Configuration" views.\n\n'
+            "Configuration is an overview of a large subset of objects within the detector. "
+            "It gives you a global view of what's currently enabled/disabled.\n\n"
+            "The System Maps give you maps of elements in the Enable/Disable Panel. "
+            "This can tell you exactly what the panels are turning on and off.\n"
+        )
+
+        # Adjustable Attributes
+        text.append("\nAdjustable Attributes\n", style="bold")
+        text.append(
+            "Finally, we can look at the adjustable attributes. Adjustable attributes are "
+            "things we want to set the value of before the run, for example, trigger rates.\n"
+        )
+
+        # Creating Configuration
+        text.append("\nCreating a Configuration\n", style="bold")
+        text.append(
+            'Once you\'re done modifying the configuration, simply press "create". '
+            "This will generate a configuration file and your terminal will tell you "
+            "how to use this with DRUNC.\n"
+        )
+
+        return text
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """
