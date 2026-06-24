766a49b5b36f05527a19e88c4296f86698977649
Henry Wallace <henry.wallace@rhul.ac.uk>
2026-06-24 09:34:10 +0100

    michal fix: error->exception and add escape to rich markup

--- src/runconf_ui/textual/screens/popup_screens.py
+++ src/runconf_ui/textual/screens/popup_screens.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Literal
 
+from rich.markup import escape
 from textual import on
 from textual.app import ComposeResult
 from textual.containers import Grid, Vertical
@@ -238,6 +239,6 @@ class ExceptionScreen(ButtonPopup):
                 ),
             ],
             info_str="Error handling config. This is likely due to incompatible config or older DAQ dependencies.\n"
-            f"The following error has been raised: \n [bold red]{error_msg}.",
+                    f"The following error has been raised: \n [bold red]{escape(error_msg)}[/bold red]",
             css_classes="pop_up quit_pop_up_grid",
         )
