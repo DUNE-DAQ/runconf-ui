ffe6847de1ac1195677b8bd3ce56430b9889488c
Marco Roda <mroda@liverpool.ac.uk>
2026-07-27 12:06:28 +0200

    Correct guard order for script

--- scripts/runconf_np02-emu_env_setup.sh
+++ scripts/runconf_np02-emu_env_setup.sh
@@ -3,8 +3,9 @@
 export APPARATUS="np02-emu"
 
 export SESSION_FILE="np02-emu-session.data.xml"
-export CONFIG_DIR="${DBT_AREA_ROOT}/np02-emu-runs"
+
 : "${DBT_AREA_ROOT:?DBT_AREA_ROOT must be set before sourcing this script}"
+export CONFIG_DIR="${DBT_AREA_ROOT}/np02-emu-runs"
 
 export OPERATION_URL="https://gitlab.cern.ch/dune-daq/online/np02-configs-operation.git"
 export BASE_URL="ssh://git@gitlab.cern.ch:7999/dune-daq/online/ehn1-daqconfigs.git"
