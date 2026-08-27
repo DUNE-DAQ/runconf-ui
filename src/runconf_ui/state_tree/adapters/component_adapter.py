88aaa2f8b170b726d10ec2afed82650b43406754
Marco Roda <marco.roda.88@gmail.com>
2026-08-27 12:52:54 +0100

    Fix Resource

--- src/runconf_ui/state_tree/adapters/component_adapter.py
+++ src/runconf_ui/state_tree/adapters/component_adapter.py
@@ -24,13 +24,13 @@ class IncludeComponent(Adapter):
 
         :param configuration: The Configuration object containing the DAL
         :param session: The session DAL object
-        :param dal: The Resource DAL object to manage
+        :param dal: The ExcludableEntity DAL object to manage
         :param label: Optional label for display purposes
-        :raises IncompatibleDalException: If the DAL is not a Resource class
+        :raises IncompatibleDalException: If the DAL is not a ExcludableEntity class
         """
-        if "Resource" not in configuration.superclasses(dal.className(), all=True):
+        if "ExcludableEntity" not in configuration.superclasses(dal.className(), all=True):
             raise IncompatibleDalException(
-                f"{dal!r} is not of class 'Resource' this means it cannot be trivially excluded/incdlude"
+                f"{dal!r} is not of class 'ExcludableEntity' this means it cannot be trivially excluded/included"
             )
         self.label = label
         super().__init__(configuration, session, dal)
