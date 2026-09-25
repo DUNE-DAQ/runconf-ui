ed86f7d37774b21fecd86a76130eff64598043f6
Marco Roda <marco.roda.88@gmail.com>
2026-09-25 18:03:11 +0100

    Address remaining comments

--- scripts/runconf_np04_env_setup.sh
+++ scripts/runconf_np04_env_setup.sh
@@ -7,5 +7,5 @@ export SESSION_FILE="np04-session.data.xml"
 : "${DBT_AREA_ROOT:?DBT_AREA_ROOT must be set before sourcing this script}"
 export CONFIG_DIR="${DBT_AREA_ROOT}/np04-runs"
 
-export OPERATION_URL="https://gitlab.cern.ch/dune-daq/online/np02-configs-operation.git"
+export OPERATION_URL="https://gitlab.cern.ch/dune-daq/online/np04-configs-operation.git"
 export BASE_URL="https://gitlab.cern.ch/dune-daq/online/ehn1-daqconfigs.git"
